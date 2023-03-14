8d5e3854df452aa101427cb0bd778a452ef66fd9
Anthony Beckford <abeckford532@gmail.com>
2023-03-14 14:47:20 -0400

    Created the sliders and labels on the BullsEye App

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 50
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,12 +17,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController (title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)"
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        // Will prompt and alert on the screen
+        let alert = UIAlertController (title: "Hello, World", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+       
+        
+        currentValue = lroundf(slider.value)
     }
 
 }
